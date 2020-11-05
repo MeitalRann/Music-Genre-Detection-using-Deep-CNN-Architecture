5b0999c37bb1f05ec09ea2d604440d0d538ddddb
Meital Rannon <54916902+MeitalRann@users.noreply.github.com>
2020-11-05 11:07:58 -0500

    Add files via upload

--- Main.py
+++ Main.py
@@ -2,8 +2,8 @@
 import os
 import subprocess
 
-extr = 0  # 1 to extract the features, 0 don't extract
-mode = 0  # 1 to train the network, 0 to use a pre-trained model
+extr = 1  # 1 to extract the features, 0 don't extract
+mode = 1  # 1 to train the network, 0 to use a pre-trained model
 prj_dir = os.getcwd()
 
 subprocess.call(["python", prj_dir+r"\\lib\\main.py",
